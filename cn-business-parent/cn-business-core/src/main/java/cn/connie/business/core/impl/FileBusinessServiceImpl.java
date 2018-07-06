00a63428db551a0c0328574689ae1c7b078ea7e3
hinge <kang.yang@shanggucai.com>
2018-07-06 18:21:04 +0800

    提交1.0版本

--- cn-business-parent/cn-business-core/src/main/java/cn/connie/business/core/impl/FileBusinessServiceImpl.java
+++ cn-business-parent/cn-business-core/src/main/java/cn/connie/business/core/impl/FileBusinessServiceImpl.java
@@ -8,14 +8,28 @@ import cn.connie.file.from.TFileFrom;
 import cn.connie.file.service.FileService;
 import cn.connie.file.to.TFileTO;
 import com.sgcai.commons.lang.utils.BeanConvertUtils;
+import net.coobird.thumbnailator.Thumbnails;
 import org.apache.commons.codec.digest.DigestUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayInputStream;
+import java.io.File;
 import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 @Service
 public class FileBusinessServiceImpl implements FileBusinessService {
+
+
+    @Value("${local.save.path}")
+    private String LOCAL_SAVE_PATH;
+    @Value("${network.access.path}")
+    private String NETWORK_ACCESS_PATH;
 
     @Autowired
     private FileService fileService;
@@ -29,7 +43,7 @@ public class FileBusinessServiceImpl implements FileBusinessService {
         } else {
             String url;
             try {
-                url = FileUtils.saveFile(fileName, data);
+                url = saveFile(fileName, data);
             } catch (IOException e) {
                 throw new CustomException("upload failed", "文件上传失败");
             }
@@ -38,6 +52,39 @@ public class FileBusinessServiceImpl implements FileBusinessService {
             tFileFrom.setUrl(url);
             tFileTO = fileService.insert(tFileFrom);
             return BeanConvertUtils.convert(tFileTO, TFileBTO.class);
+        }
+    }
+
+    /**
+     * 保存文件到服务器中，返回文件访问路径
+     *
+     * @param fileName
+     * @param data
+     * @return
+     * @throws IOException
+     */
+    private String saveFile(String fileName, byte[] data) throws IOException {
+        StringBuilder sb = new StringBuilder();
+        String month = new SimpleDateFormat("yyMM").format(new Date());
+        String newFileName = FileUtils.getUniqueFileName(fileName);
+        String dir = sb.append(LOCAL_SAVE_PATH).append(month).append("/").toString();
+        sb.append(newFileName);
+
+        File file = new File(dir);
+        if (!file.exists()) {
+            file.mkdirs();
+        }
+        ByteArrayInputStream in = null;
+        try {
+            in = new ByteArrayInputStream(data);
+            Image src = Toolkit.getDefaultToolkit().createImage(data);
+            BufferedImage image = FileUtils.toBufferedImage(src);
+            Thumbnails.of(image).scale(1f).outputQuality(1f).toFile(sb.toString());
+            return NETWORK_ACCESS_PATH + month + "/" + newFileName;
+        } finally {
+            if (in != null) {
+                in.close();
+            }
         }
     }
 }
