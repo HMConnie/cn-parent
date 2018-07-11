88a3b179f4c9ba447d52b1cbe020d73eaeb41ee4
hinge <kang.yang@shanggucai.com>
2018-07-11 11:04:51 +0800

    提交1.0版本

--- cn-job-parent/cn-job-core/src/main/java/cn/connie/job/boot/jobhandler/PushJobHandler.java
+++ cn-job-parent/cn-job-core/src/main/java/cn/connie/job/boot/jobhandler/PushJobHandler.java
@@ -1,6 +1,8 @@
 package cn.connie.job.boot.jobhandler;
 
 import cn.connie.common.model.PushMessage;
+import cn.jiguang.common.resp.APIConnectionException;
+import cn.jiguang.common.resp.APIRequestException;
 import cn.jpush.api.JPushClient;
 import cn.jpush.api.push.PushResult;
 import cn.jpush.api.push.model.Options;
@@ -11,15 +13,15 @@ import cn.jpush.api.push.model.notification.AndroidNotification;
 import cn.jpush.api.push.model.notification.IosNotification;
 import cn.jpush.api.push.model.notification.Notification;
 import com.alibaba.fastjson.JSON;
-import com.sgcai.zookeeper.lib.DistributedLock;
 import com.xxl.job.core.biz.model.ReturnT;
 import com.xxl.job.core.handler.IJobHandler;
 import com.xxl.job.core.handler.annotation.JobHandler;
-import org.apache.commons.lang3.StringUtils;
+import org.apache.commons.lang.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.data.redis.core.BoundListOperations;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Component;
 
@@ -42,55 +44,44 @@ public class PushJobHandler extends IJobHandler {
     @Autowired
     private StringRedisTemplate redisTemplate;
 
-    @Autowired
-    private DistributedLock distributedLock;
 
     @Override
     public ReturnT<String> execute(String s) throws Exception {
-        String json = redisTemplate.boundListOps(PUSH_STORE_KEY).leftPop();
-        if (StringUtils.isBlank(json)) {
-            return ReturnT.SUCCESS;
-        } else {
-            boolean isLock = false;
-            try {
-                isLock = distributedLock.getLock(this.getClass().getName());
-                if (isLock) {
-                    PushMessage message = JSON.parseObject(json, PushMessage.class);
-                    Map<String, String> params = new HashMap<>();
-                    params.put("refId", message.getRefId());
-                    params.put("messageType", message.getMessageType().name());
-
-                    PushPayload payload = PushPayload.newBuilder().setPlatform(Platform.all())
-                            .setAudience(Audience.alias(message.getUserId()))
-                            .setNotification(Notification.newBuilder()
-                                    .addPlatformNotification(AndroidNotification.newBuilder()
-                                            .addExtras(params)
-                                            .setAlert(message.getContent())
-                                            .build())
-                                    .addPlatformNotification(
-                                            IosNotification.newBuilder()
-                                                    .addExtras(params)
-                                                    .setBadge(1)
-                                                    .setAlert(message.getContent())
-                                                    .setSound(IOS_SOUND)
-                                                    .build()).build())
-                            .setOptions(Options.newBuilder()
-                                    .setApnsProduction(true)
-                                    .build()).build();
-                    PushResult result = jPushClient.sendPush(payload);
-                    LOGGER.info("jpush send status:", result.statusCode);
-                    return ReturnT.SUCCESS;
-                }
-            } catch (Exception e) {
-                LOGGER.error(e.getMessage(), e);
-            } finally {
-                if (isLock) {
-                    distributedLock.releaseLock();
-                }
-            }
-            return ReturnT.FAIL;
+        BoundListOperations<String, String> listOps = redisTemplate.boundListOps(PUSH_STORE_KEY);
+        Long listOpsSize = listOps.size();
+        for (int i = 0; i < listOpsSize; i++) {
+            String json = listOps.leftPop();
+            pushMessage(json);
         }
+        return ReturnT.SUCCESS;
+    }
 
+    private void pushMessage(String json) throws APIConnectionException, APIRequestException {
+        if (StringUtils.isBlank(json)) return;
 
+        PushMessage message = JSON.parseObject(json, PushMessage.class);
+        Map<String, String> params = new HashMap<>();
+        params.put("refId", message.getRefId());
+        params.put("messageType", message.getMessageType().name());
+
+        PushPayload payload = PushPayload.newBuilder().setPlatform(Platform.all())
+                .setAudience(Audience.alias(message.getUserId()))
+                .setNotification(Notification.newBuilder()
+                        .addPlatformNotification(AndroidNotification.newBuilder()
+                                .addExtras(params)
+                                .setAlert(message.getContent())
+                                .build())
+                        .addPlatformNotification(
+                                IosNotification.newBuilder()
+                                        .addExtras(params)
+                                        .setBadge(1)
+                                        .setAlert(message.getContent())
+                                        .setSound(IOS_SOUND)
+                                        .build()).build())
+                .setOptions(Options.newBuilder()
+                        .setApnsProduction(true)
+                        .build()).build();
+        PushResult result = jPushClient.sendPush(payload);
+        LOGGER.info("jpush send status:", result.statusCode);
     }
 }
--- cn-job-parent/cn-job-core/src/test/java/cn/connie/user/core/DBTests.java
+++ cn-job-parent/cn-job-core/src/test/java/cn/connie/user/core/DBTests.java
@@ -4,10 +4,15 @@ import cn.connie.common.model.PushMessage;
 import cn.connie.job.service.PushRemoteService;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.BoundListOperations;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import javax.annotation.Resource;
+import java.util.ArrayList;
+import java.util.List;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(value = {"classpath:applicationContext.xml", "classpath:redis-config.xml"})
@@ -20,10 +25,27 @@ public class DBTests {
     @Resource
     private PushRemoteService pushRemoteService;
 
+    @Autowired
+    private StringRedisTemplate redisTemplate;
 
     @Test
     public void testPush() throws Exception {
-        pushRemoteService.push("ab29ff542ba940eda89d7ba6202e69e8", null, "22222", PushMessage.PushMessageType.OTHER, null);
+        BoundListOperations<String, String> listOps = redisTemplate.boundListOps("PUSH_STORE_KEY");
+        listOps.rightPush("1");
+        listOps.rightPush("2");
+        listOps.rightPush("3");
+//        String s1 = listOps.leftPop();
+//        String s2 = listOps.leftPop();
+//        String s3 = listOps.leftPop();
+//        System.out.println(s1);
+//        System.out.println(s2);
+//        System.out.println(s3);
+        Long size = listOps.size();
+        for (int i = 0; i < size; i++) {
+            String s = listOps.leftPop();
+            System.out.println(s);
+        }
+
     }
 
 
